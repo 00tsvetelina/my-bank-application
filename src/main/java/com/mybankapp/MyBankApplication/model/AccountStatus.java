653c9e5588feb6d3a3d0eed884dc441c0f8179bb
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2024-06-06 16:39:06 +0300

    created base package structure of application; added main entities and column fields

--- src/main/java/com/mybankapp/MyBankApplication/model/AccountStatus.java
+++ src/main/java/com/mybankapp/MyBankApplication/model/AccountStatus.java
@@ -1,2 +1,6 @@
-package com.mybankapp.MyBankApplication.model;public enum AccountStatus {
+package com.mybankapp.MyBankApplication.model;
+
+public enum AccountStatus {
+    ACTIVE,
+    INACTIVE
 }
--- src/main/java/com/mybankapp/MyBankApplication/service/AccountService.java
+++ src/main/java/com/mybankapp/MyBankApplication/service/AccountService.java
@@ -1,2 +1,71 @@
-package com.mybankapp.MyBankApplication.service;public class AccountService {
+package com.mybankapp.MyBankApplication.service;
+
+import com.mybankapp.MyBankApplication.model.Account;
+import com.mybankapp.MyBankApplication.model.AccountStatus;
+import com.mybankapp.MyBankApplication.model.Transaction;
+import com.mybankapp.MyBankApplication.model.TransactionType;
+import com.mybankapp.MyBankApplication.repository.AccountRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    // get all accounts
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    // get account by id
+    public Account getAccountById(Long accountId) {
+        return accountRepository.getAccountById(accountId)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                String.format("Cannot find account with id: %d", accountId)
+                        )
+                );
+    }
+
+    // save account
+    @Transactional
+    public Account saveAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    // update account status
+    @Transactional
+    public Account updateAccountStatus(Long accountId) {
+        Account account = getAccountById(accountId);
+
+        AccountStatus currentStatus = account.getAccountStatus();
+        AccountStatus newStatus =
+                currentStatus == AccountStatus.ACTIVE ? AccountStatus.INACTIVE : AccountStatus.ACTIVE;
+        account.setAccountStatus(newStatus);
+
+        return accountRepository.save(account);
+    }
+
+    // update account balance
+    @Transactional
+    public Account updateAccountBalance(Long accountId ,Transaction transaction) {
+        Account account = getAccountById(accountId);
+        Double balance = account.getBalance();
+        TransactionType transactionType = transaction.getTransactionType();
+
+        switch (transactionType) {
+            case TRANSFER, WITHDRAWAL -> balance -= transaction.getAmount();
+            case DEPOSIT -> balance += transaction.getAmount();
+        }
+
+        account.setBalance(balance);
+        return accountRepository.save(account);
+
+    }
+
 }

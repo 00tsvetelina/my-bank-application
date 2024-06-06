cc557f7965f98088a8e795cab3ae19159e32a9af
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2024-06-06 13:16:38 +0300

    completed the entities and fixed the relational mappings; created repositories for each entity file; connected app to db

--- src/main/java/com/mybankapp/MyBankApplication/model/User.java
+++ src/main/java/com/mybankapp/MyBankApplication/model/User.java
@@ -3,29 +3,31 @@ package com.mybankapp.MyBankApplication.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name="user")
-public class User {
+@Table(name="\"user\"")
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "accounts")
+    @OneToMany(mappedBy = "user")
     private List<Account> accounts;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
 }
